2bd9ecdd93db5a9cf1dddbc69d70b419f9e08765
Victoria Ferrarese <89160221+victoriaferrarese@users.noreply.github.com>
2022-10-07 17:27:59 -0300

    chutes
    o Código imprime corretamente as letras chutadas e os _

--- JogoDaForcaEmC/main.c
+++ JogoDaForcaEmC/main.c
@@ -10,18 +10,33 @@ int main(void){
     int acertou = 0;
     int enforcou = 0;
 
+    char chutes[26]; //array que registra todos os chutes feitos pelo usuário.
+    int tentativas = 0;
     do{
-        char chute;
-        scanf("%c", &chute);
 
-        for(int i = 0; i < strlen(palavrasecreta); i++){
-            if (chute == palavrasecreta[i]){
-                acertou = 1;
-                printf("posicao %d\n",i);
+        for(int i = 0; i < strlen(palavrasecreta); i++){ //percorre a palavrasecreta
+            int achou = 0; //variável de controle para a verificação do chute
+            for(int j = 0; j < tentativas; j++){
+
+                if(chutes[j] == palavrasecreta[i]){
+                    achou = 1; //caso o chute esteja certo a variável de controle vira verdadeira (boolean)
+                    break;
+                }
+            }
+            if(achou){
+                printf("%c ",palavrasecreta[i]);
+            }else {
+                printf("_ ");
             }
         }
-        if (acertou == 0){
-            enforcou = 1;
-        }
+        printf("\n");
+
+        char chute;
+        printf("\nChute uma letra: ");
+        scanf("%c", &chute);
+
+        chutes[tentativas]= chute;
+        tentativas++;
+
     }while(!acertou && !enforcou); //lógica booleana : enquanto a sentença for verdadeira o loop acontece.
 }
