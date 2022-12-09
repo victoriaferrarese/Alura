86db995895d7026a4c39a1ff8ccc261343957c7d
Victoria Ferrarese <89160221+victoriaferrarese@users.noreply.github.com>
2022-12-09 17:22:48 -0300

    selecionarPalavra()
    add :
    * selecionarPalavra()
    * forca.h
    * palavras.txt

---
+++ JogoDaForcaEmC/forca.h
@@ -0,0 +1,9 @@
+//Header file : contém as assinaturas do codigo
+
+void abertura();
+void selecionarPalavra();
+void receberChute();
+int verificarChute(char letra);
+void imprimirForca();
+int enforcou();
+int ganhou();
--- JogoDaForcaEmC/main.c
+++ JogoDaForcaEmC/main.c
@@ -1,40 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include "forca.h"
 
 //Variaveis globais
 char palavrasecreta[20];
 int chutesRealizados = 0;
 char chutes[26];
-char chute;
-
-//Assinaturas
-void abertura();
-void receberChute();
-int verificarChute(char letra);
-void imprimirForca();
-int enforcou();
-int ganhou();
 
 int main(void){
 
-    sprintf(palavrasecreta, "HIPOPOTAMO");
     abertura();
-    
+    selecionarPalavra();
+    imprimirForca();
 
     do{
         receberChute();
         imprimirForca();
 
-    }while(!ganhou() && !enforcou()); //condicao booleana 
+    }while(!ganhou() && !enforcou()); //condicao booleana: enquanto ganhou() = 0 (falso) e enforcou = 0 (falso)  
     
 }
+//seleciona a palavra secreta 
+void selecionarPalavra(){
+    
+    FILE* f; //declarando a variavel f : tipo FILE. 
 
+    f = fopen("palavras.txt","r"); //funcao que abre um arquivo no diretorio. Essa função retorna um ponteiro de arquivo. fopen("nomedoarquivo", "r = leitura")
+
+    int qtddepalavras; //variavel que recebera a primeira linha do arquivo f.
+    fscanf(f, "%d", &qtddepalavras); //funcao que le um alquivo. fscanf(arquivo, "tipodedado", &variavelemqueseraarmazenado)
+    
+    //**************************************************************************************
+    srand(time(NULL));
+
+    int numeroSorteado = rand() % qtddepalavras; //sorteia o numero da linha arquivo f 
+
+    for(int i = 0; i <= numeroSorteado; i++){ 
+        fscanf(f, "%s", palavrasecreta); //le a string (ate \n)
+    }
+
+    fclose(f); //fecha o arquivo
+}
+
+//Imprime o titulo do jogo
 void abertura(){
 
     printf("*******************************\n");
     printf("\tJOGO DA FORCA\n");
-    printf("*******************************\n");
+    printf("*******************************\n\n");
 }
 
 //Recebe o chute e adiciona no array de chutes 
@@ -75,9 +90,12 @@ void imprimirForca(){
 }
 //contabiliza erros e verifica se o limite de erros foi atingido
 int enforcou(){ 
+
     int erros = 0; //max = 5
-    for(int i = 0; i < chutesRealizados; i++){ 
-        int existe = 0;//Variavel booleana (falso)       
+
+    for(int i = 0; i < chutesRealizados; i++){  
+        int existe = 0;//Variavel booleana (falso)
+
         for(int j = 0; j < strlen(palavrasecreta); j++){ //percorre o array da palavra secreta
             if (chutes[i] == palavrasecreta[j]){
                 existe = 1; //variavel booleana (verdadeiro)
@@ -91,7 +109,7 @@ int enforcou(){
 //verifica se todas as letras da palavra secreta foram acertadas
 int ganhou(){
     for(int i = 0; i < strlen(palavrasecreta); i++){
-        if (verificarChute(palavrasecreta[i])){
+        if (!verificarChute(palavrasecreta[i])){
             return 0; //se chute = verdadeiro (1), retorna 0;
         }
     }
