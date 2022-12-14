84ad2dfc1ff102804d4c9db95b7df54590cc310e
Victoria Ferrarese <89160221+victoriaferrarese@users.noreply.github.com>
2022-12-14 17:08:21 -0300

    malloc()
    add :
    * Função malloc()

--- PacMan/main.c
+++ PacMan/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
+    int linhas;
+    int colunas;
 
-    //mapa.txt sera armazenado em uma matriz 
+   //mapa.txt sera armazenado em uma matriz 
     char mapa[5][11]; 
     /*as dimensoes do mapa sao 5 x 10. Porem a ultima posicao de uma matriz eh sempre o \0, assim como um array, dessa forma eh necessario adicionar 1 na posicao da coluna da matriz */
+
+
+int main(void){
 
     FILE* f;
     f = fopen("mapa.txt", "r");
@@ -14,6 +18,14 @@ int main(void){
         printf("Banco de dados indisponivel\n");
         exit(1);
     }
+    
+    fscanf(f, "%d %d", &linhas, &colunas);
+    fprintf("Linhas %d colunas %d\n", linhas, colunas);
+
+    //Alocacao dinamica de memoria
+
+    int** v = malloc(sizeof(int*) * 5); // alocando espaco para 5 ponteiros de interios (5 arrays, que representam as linhas). Como estou alocando ponteiros, a variavel v é um ponteiro de ponteiro (um ponteiro de arrays).
+
 
     for(int i = 0; i < 5; i++){
     fscanf(f, "%s", mapa[i]); 
