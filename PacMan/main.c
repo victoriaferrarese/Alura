664ad604a17bd65011caab1959c3eb188036caf4
Victoria Ferrarese <89160221+victoriaferrarese@users.noreply.github.com>
2022-12-15 17:06:41 -0300

    malloc()
    learned:
    * função malloc e como armazenar uma matriz de forma dinamica na memoria

--- PacMan/main.c
+++ PacMan/main.c
@@ -5,9 +5,8 @@
     int colunas;
 
    //mapa.txt sera armazenado em uma matriz 
-    char mapa[5][11]; 
-    /*as dimensoes do mapa sao 5 x 10. Porem a ultima posicao de uma matriz eh sempre o \0, assim como um array, dessa forma eh necessario adicionar 1 na posicao da coluna da matriz */
-
+  
+    char** mapa; //ponteiro de ponteiro
 
 int main(void){
 
@@ -20,20 +19,35 @@ int main(void){
     }
     
     fscanf(f, "%d %d", &linhas, &colunas);
-    fprintf("Linhas %d colunas %d\n", linhas, colunas);
+    printf("Linhas %d colunas %d\n", linhas, colunas);
 
     //Alocacao dinamica de memoria
+    mapa = malloc(sizeof(char*) * linhas); 
+    /*alocando espaco para linhas * ponteiros de caracteres ( arrays, que representam as linhas). Como estou alocando ponteiros, mapa é um ponteiro de ponteiro (um ponteiro de arrays), sendo assim deve ser declarada como char** */
 
-    int** v = malloc(sizeof(int*) * 5); // alocando espaco para 5 ponteiros de interios (5 arrays, que representam as linhas). Como estou alocando ponteiros, a variavel v é um ponteiro de ponteiro (um ponteiro de arrays).
-
+    for(int i = 0; i < linhas; i++){
+        mapa[i] = malloc(sizeof(char) * (colunas + 1));
+    }
+    /* Dentro de cada um dos  arrays que foram alocados na memoria, atraves desse loop, estou alocando espaco para colunas * caracteres */
+    /*a ultima posicao de uma matriz eh sempre o \0 (assim como um array), dessa forma eh necessario adicionar 1 na posicao da coluna da matriz */
 
     for(int i = 0; i < 5; i++){
-    fscanf(f, "%s", mapa[i]); 
-    /*o fscanf le o arquivo e armazena a informação em um array. Porem como vou armazenar o mapa em uma matriz, preciso passar para a função apenas uma dimensao dessa matriz (linha). Assim, o mapa sera armazenado na matriz linha por linha */
+        fscanf(f, "%s", mapa[i]); 
+    /*o fscanf le o arquivo e armazena a informação em uma variavel/array. Porem como vou armazenar o mapa em uma matriz, preciso passar para a função apenas uma dimensao dessa matriz (linha). Assim, o mapa sera armazenado na matriz linha por linha.
+    obs: Como um array é naturalmente um ponteiro nao ha necessidade de escrever &mapa[i]*/
     }
-
     for(int i = 0; i < 5; i++){
         printf("%s\n", mapa[i]);
     }
+
+    /* Como foram usados dois malloc(), devemos usar dois free(), um esvazia o espaco dentro de cada linha e o outro esvazia as proprias linhas da matriz. 
+    obs: free() deve estar no fim do codigo todo*/
+
+    for(int i = 0; i < 5; i++){
+        //Para cada linha v[]
+        free(mapa[i]);   
+    }
+    //Para o ponteiro v**
+    free(mapa);
     
 }
